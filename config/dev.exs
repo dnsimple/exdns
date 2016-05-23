05de09fdcd1825e03a35093f31003764e0181007
Anthony Eden <me@anthonyeden.com>
2016-05-23 14:38:17 +0200

    Remove option that is no longer available.

--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,5 @@
 use Mix.Config
 
-config :exdns, port: 8053
 config :exdns, catch_exceptions: false
 config :exdns, zone_file: "priv/test.zones.json"
 
--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,5 @@ use Mix.Config
 
 config :logger, level: :info
 
-config :exdns, port: 8053
 config :exdns, catch_exceptions: false
 config :exdns, zone_file: "priv/test.zones.json"
