70013e7e92b71f584797e480d016370a569ffc26
Anthony Eden <me@anthonyeden.com>
2018-02-24 16:47:40 -0500

    Removed GenEvent usage

--- test/exdns/events_test.exs
+++ test/exdns/events_test.exs
@@ -3,18 +3,18 @@ defmodule Exdns.EventsTest do
   require Logger
 
   defmodule EventCapture do
-    use GenEvent
+    use GenServer
 
     def init(_) do
       {:ok, []}
     end
 
-    def handle_event(message, messages) do
-      {:ok, [message|messages]}
+    def handle_cast(message, messages) do
+      {:noreply, [message|messages]}
     end
 
-    def handle_call(:messages, messages) do
-      {:ok, Enum.reverse(messages), messages}
+    def handle_call(:messages, _from, messages) do
+      {:reply, Enum.reverse(messages), messages}
     end
   end
 
@@ -23,16 +23,8 @@ defmodule Exdns.EventsTest do
   end
 
   test "add handler and notify" do
-    Exdns.Events.add_handler(Exdns.EventsTest.EventCapture, [])
+    {:ok, pid} = Exdns.Events.add_handler(Exdns.EventsTest.EventCapture, [])
     Exdns.Events.notify(:test)
-    assert GenEvent.call(Exdns.Events, Exdns.EventsTest.EventCapture, :messages) == [:test]
-  end
-
-  test "handle start_severs event" do
-    Exdns.Server.Supervisor.stop()
-    Exdns.Events.add_handler(Exdns.Events, [])
-    assert GenEvent.call(Exdns.Events, Exdns.Events, :get_servers_running) == false
-    Exdns.Events.notify(:start_servers)
-    assert GenEvent.call(Exdns.Events, Exdns.Events, :get_servers_running) == true
+    assert GenServer.call(pid, :messages) == [:test]
   end
 end
